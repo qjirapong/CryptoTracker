3a42ebe1585908dfb09540bae852b02c699003ff
Jirapong Fuangkajonsak <34644844+qjirapong@users.noreply.github.com>
2026-07-07 19:46:42 +0700

    Added text content in CoinListItem

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/components/CoinListItem.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/components/CoinListItem.kt
@@ -2,21 +2,29 @@ package com.plcoding.cryptotracker.crypto.presentation.coin_list.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.plcoding.cryptotracker.crypto.domain.Coin
 import com.plcoding.cryptotracker.crypto.presentation.model.CoinUI
 import com.plcoding.cryptotracker.crypto.presentation.model.toCoinUI
@@ -27,6 +35,12 @@ fun CoinListItem(
     coinUI: CoinUI,
     onClick: () -> Unit,
     modifier: Modifier = Modifier) {
+    val textColor = if (isSystemInDarkTheme()){
+        Color.White
+    }
+    else{
+        Color.Black
+    }
     Row(
         modifier = modifier
             .clickable(onClick = onClick)
@@ -39,10 +53,35 @@ fun CoinListItem(
             tint = MaterialTheme.colorScheme.primary,
             modifier = Modifier.size(85.dp)
         )
+        Column(
+            modifier = Modifier.weight(1f),
+            verticalArrangement = Arrangement.Center) {
+            Text(
+                text = coinUI.symbol,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+            Text(
+                text = coinUI.name,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Light,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.End) {
+            Text(
+                text = "$ ${coinUI.priceUSD.formatted}",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+        }
     }
 }
 
-@Preview
 @PreviewLightDark
 @Composable
 private fun CoinListItemPreview(){
