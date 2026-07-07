0d9a614bb6eeb95ad70cfbce66a7ba8c853ca6ad
Jirapong Fuangkajonsak <34644844+qjirapong@users.noreply.github.com>
2026-07-07 18:47:53 +0700

    Some code format in Theme.kt

--- app/src/main/java/com/plcoding/cryptotracker/ui/theme/Theme.kt
+++ app/src/main/java/com/plcoding/cryptotracker/ui/theme/Theme.kt
@@ -238,13 +238,12 @@ private val highContrastDarkColorScheme = darkColorScheme(
     surfaceContainerHighest = surfaceContainerHighestDarkHighContrast,
 )
 
+// Dynamic color is available on Android 12+
 @Composable
 fun CryptoTrackerTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
-) {
+    content: @Composable () -> Unit) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
