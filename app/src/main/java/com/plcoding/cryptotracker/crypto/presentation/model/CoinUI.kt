171dc85b493a967eafcc43c605d9818d5f227896
Jirapong Fuangkajonsak <34644844+qjirapong@users.noreply.github.com>
2026-07-07 19:31:56 +0700

    Added number formatting and inter-model communication
    CoinUI.kt

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/model/CoinUI.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/model/CoinUI.kt
@@ -1,6 +1,10 @@
 package com.plcoding.cryptotracker.crypto.presentation.model
 
 import androidx.annotation.DrawableRes
+import com.plcoding.cryptotracker.crypto.domain.Coin
+import com.plcoding.cryptotracker.util.getDrawableIdForCoin
+import java.text.NumberFormat
+import java.util.Locale
 
 data class CoinUI(
     val id: String,
@@ -17,3 +21,28 @@ data class DisplayableNumber(
     val value: Double,
     val formatted: String
 )
+
+
+fun Coin.toCoinUI(): CoinUI {
+    return CoinUI(
+        id = id,
+        rank = rank,
+        name = name,
+        symbol = symbol,
+        marketCapUSD = marketCapUSD.toDisplayableNumber(),
+        priceUSD = priceUSD.toDisplayableNumber(),
+        changePercent24HR = changePercent24HR.toDisplayableNumber(),
+        iconRes = getDrawableIdForCoin(symbol)
+    )
+}
+
+fun Double.toDisplayableNumber(): DisplayableNumber {
+    val formatter = NumberFormat.getNumberInstance(Locale.getDefault()).apply {
+        minimumFractionDigits = 2
+        maximumFractionDigits = 2
+    }
+    return DisplayableNumber(
+        value = this,
+        formatted = formatter.format(this)
+    )
+}
