8a2a946c26930f13e34627784ef6727c3961fc9e
Jirapong Fuangkajonsak <34644844+qjirapong@users.noreply.github.com>
2026-07-08 16:30:31 +0700

    Create PriceChange.kt
    Price Change capsule

---
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/components/PriceChange.kt
@@ -0,0 +1,105 @@
+package com.plcoding.cryptotracker.crypto.presentation.coin_list.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Code
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import com.plcoding.cryptotracker.crypto.presentation.model.DisplayableNumber
+import com.plcoding.cryptotracker.ui.theme.CryptoTrackerTheme
+import com.plcoding.cryptotracker.ui.theme.greenBackground
+
+@Composable
+fun PriceChange(
+    change: DisplayableNumber,
+    modifier: Modifier = Modifier) {
+    val textColor = if (change.value < 0.0){
+        MaterialTheme.colorScheme.onErrorContainer
+    }
+    else if (change.value == 0.0){
+        Color.Yellow
+    }
+    else{
+        Color.Green
+    }
+    val backgroundColor = if (change.value < 0.0){
+        MaterialTheme.colorScheme.errorContainer
+    }
+    else if (change.value == 0.0){
+        MaterialTheme.colorScheme.outlineVariant
+    }
+    else{
+        greenBackground
+    }
+
+    Row(modifier = modifier
+        .clip(RoundedCornerShape(100f))
+        .background(backgroundColor)
+        .padding(horizontal = 8.dp, vertical = 4.dp)) {
+        Icon(
+            imageVector = if (change.value < 0.0) {
+                Icons.Default.KeyboardArrowDown
+            }
+            else if (change.value == 0.0){
+                Icons.Default.Code
+            }
+            else {
+                Icons.Default.KeyboardArrowUp
+            },
+            null,
+            modifier = Modifier.size(20.dp),
+            tint = textColor
+        )
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun PriceNoChangePreview() {
+    CryptoTrackerTheme {
+        PriceChange(
+            change = DisplayableNumber(
+                value = 0.0,
+                formatted = "0.00"
+            )
+        )
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun PriceChangeDownPreview() {
+    CryptoTrackerTheme {
+        PriceChange(
+            change = DisplayableNumber(
+                value = -10.0,
+                formatted = "-10.00"
+            )
+        )
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun PriceChangeUpPreview() {
+    CryptoTrackerTheme {
+        PriceChange(
+            change = DisplayableNumber(
+                value = 10.0,
+                formatted = "10.00"
+            )
+        )
+    }
+}
