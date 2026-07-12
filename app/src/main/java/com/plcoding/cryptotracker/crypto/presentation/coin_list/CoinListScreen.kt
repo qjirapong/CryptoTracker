dde5c2a8c8d3b4d2db4b2dd00c4ff511093bd91f
Jirapong Fuangkajonsak <34644844+qjirapong@users.noreply.github.com>
2026-07-12 14:12:10 +0700

    Create CoinListScreen.kt

---
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/CoinListScreen.kt
@@ -0,0 +1,9 @@
+package com.plcoding.cryptotracker.crypto.presentation.coin_list
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CoinListScreen(modifier: Modifier = Modifier) {
+    
+}
