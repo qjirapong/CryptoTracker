0e6096f199dd3f57aade04a5321d77b7f38c0f48
Jirapong Fuangkajonsak <34644844+qjirapong@users.noreply.github.com>
2026-07-20 15:15:02 +0700

    Create CoinListViewModel.kt
    And loadCoinList function

---
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/CoinListViewModel.kt
@@ -0,0 +1,42 @@
+package com.plcoding.cryptotracker.crypto.presentation.coin_list
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.plcoding.cryptotracker.core.domain.util.onError
+import com.plcoding.cryptotracker.core.domain.util.onSuccess
+import com.plcoding.cryptotracker.crypto.domain.CoinDataSource
+import com.plcoding.cryptotracker.crypto.presentation.model.toCoinUI
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class CoinListViewModel(private val coinDataSource: CoinDataSource): ViewModel() {
+    //Private, modifiable
+    private val _state = MutableStateFlow(CoinListState())
+    //Public, read-only
+    val state = _state.asStateFlow()
+
+    private fun loadCoinList(){
+        viewModelScope.launch{
+            _state.update { it.copy(isLoading = true) }
+            coinDataSource.getCoinList()
+                .onSuccess { coinList ->
+                _state.update {
+                    it.copy(
+                        isLoading = false,
+                        coinList = coinList.map { coin -> coin.toCoinUI() }
+                    )
+                }
+            }.onError {
+                _state.update {
+                    it.copy(
+                        isLoading = false,
+                        coinList = emptyList()
+                    )
+                }
+            }
+        }
+    }
+}
