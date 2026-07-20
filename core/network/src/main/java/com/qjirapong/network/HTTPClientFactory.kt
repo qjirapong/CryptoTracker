ca1fdf5c211cb870c7dd0638b010bf6d28908015
Jirapong Fuangkajonsak <34644844+qjirapong@users.noreply.github.com>
2026-07-20 23:38:02 +0700

    Added token param
    HTTPClientFactory in Network module

--- core/network/src/main/java/com/qjirapong/network/HTTPClientFactory.kt
+++ core/network/src/main/java/com/qjirapong/network/HTTPClientFactory.kt
@@ -8,12 +8,14 @@ import io.ktor.client.plugins.logging.ANDROID
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.header
+import io.ktor.http.HttpHeaders
 import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
 object HTTPClientFactory {
-    fun create(engine: HttpClientEngine): HttpClient {
+    fun create(engine: HttpClientEngine, authToken: String? = null): HttpClient {
         return HttpClient(engine){
             install(Logging){
                 level = LogLevel.ALL
@@ -28,6 +30,9 @@ object HTTPClientFactory {
             }
             defaultRequest {
                 contentType(io.ktor.http.ContentType.Application.Json)
+                authToken?.let { token ->
+                    header(HttpHeaders.Authorization, "Bearer $token")
+                }
             }
         }
     }
