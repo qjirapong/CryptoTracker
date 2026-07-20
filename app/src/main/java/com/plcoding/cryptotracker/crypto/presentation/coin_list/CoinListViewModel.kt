116950a272a9b9bf9078a62f1fe1cd4a1ef50b16
Jirapong Fuangkajonsak <34644844+qjirapong@users.noreply.github.com>
2026-07-20 15:52:26 +0700

    Added onStart and stateIn calls for immutable state
    onStart's function will call at the start of the the state. And, the stateIn function for indicating the timeout as well as initial state

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/CoinListViewModel.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/CoinListViewModel.kt
@@ -7,8 +7,11 @@ import com.plcoding.cryptotracker.core.domain.util.onSuccess
 import com.plcoding.cryptotracker.crypto.domain.CoinDataSource
 import com.plcoding.cryptotracker.crypto.presentation.model.toCoinUI
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
@@ -16,7 +19,13 @@ class CoinListViewModel(private val coinDataSource: CoinDataSource): ViewModel()
     //Private, modifiable
     private val _state = MutableStateFlow(CoinListState())
     //Public, read-only
-    val state = _state.asStateFlow()
+    val state = _state.onStart {
+        loadCoinList()
+    }.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5000L),
+        CoinListState()
+    )
 
     private fun loadCoinList(){
         viewModelScope.launch{
