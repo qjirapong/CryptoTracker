6c78ae2142ffb5c44f89b444e9a3ed9bb73e8747
Jirapong Fuangkajonsak <34644844+qjirapong@users.noreply.github.com>
2026-07-21 16:27:16 +0700

    Added runtime lib for LaunchedEffect

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.compose.runtime)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
 
