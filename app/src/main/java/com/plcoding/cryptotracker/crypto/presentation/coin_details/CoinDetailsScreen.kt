c8e1e9a7d34dc05dc75b06e20f2efa540ed8f802
Jirapong Fuangkajonsak <34644844+qjirapong@users.noreply.github.com>
2026-07-22 00:21:08 +0700

    Added icon and text for coin details screen

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_details/CoinDetailsScreen.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_details/CoinDetailsScreen.kt
@@ -6,19 +6,25 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import com.plcoding.cryptotracker.crypto.presentation.coin_list.CoinListState
 import com.plcoding.cryptotracker.crypto.presentation.coin_list.components.previewCoin
 import com.plcoding.cryptotracker.ui.theme.CryptoTrackerTheme
 import com.plcoding.cryptotracker.ui.theme.dimens
+import com.plcoding.cryptotracker.ui.theme.rememberAppTypography
+import com.plcoding.cryptotracker.util.AutoSizeText
 
 @Composable
 fun CoinDetailsScreen(state: CoinListState,
@@ -29,6 +35,7 @@ fun CoinDetailsScreen(state: CoinListState,
     else{
         Color.Black
     }
+    val typography = rememberAppTypography()
     if (state.isLoading){
         Box(
             modifier = modifier.fillMaxSize(),
@@ -44,7 +51,17 @@ fun CoinDetailsScreen(state: CoinListState,
                 .verticalScroll(rememberScrollState())
                 .padding(MaterialTheme.dimens.spaceMedium),
             horizontalAlignment = Alignment.CenterHorizontally) {
-
+            Icon(
+                imageVector = ImageVector.vectorResource(id = coin.iconRes),
+                contentDescription = coin.name,
+                modifier = Modifier.size(MaterialTheme.dimens.coinIconSize),
+                tint = MaterialTheme.colorScheme.primary
+            )
+            AutoSizeText(
+                text = coin.name,
+                adaptiveStyle = typography.bodyLarge,
+                color = contentColor
+            )
         }
     }
 }
