5800bb2eef804b6d9c6a28deb100b8be3e54a9e4
Jirapong Fuangkajonsak <34644844+qjirapong@users.noreply.github.com>
2026-07-21 18:46:11 +0700

    Migrate LaunchedEffect call to MainActivity
    LaunchedEffect that is in ObserveAsEvents

--- app/src/main/java/com/plcoding/cryptotracker/MainActivity.kt
+++ app/src/main/java/com/plcoding/cryptotracker/MainActivity.kt
@@ -6,17 +6,34 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.repeatOnLifecycle
+import com.plcoding.cryptotracker.core.presentation.util.ObserveAsEvents
+import com.plcoding.cryptotracker.core.presentation.util.toUiText
+import com.plcoding.cryptotracker.crypto.presentation.coin_list.CoinListEvent
 import com.plcoding.cryptotracker.crypto.presentation.coin_list.CoinListScreen
 import com.plcoding.cryptotracker.crypto.presentation.coin_list.CoinListState
 import com.plcoding.cryptotracker.crypto.presentation.coin_list.CoinListViewModel
 import com.plcoding.cryptotracker.ui.theme.CryptoTrackerTheme
+import com.plcoding.cryptotracker.ui.theme.dimens
+import com.plcoding.cryptotracker.ui.theme.rememberAppTypography
+import com.plcoding.cryptotracker.util.AutoSizeText
 import org.koin.androidx.compose.koinViewModel
 
 class MainActivity : ComponentActivity() {
@@ -25,12 +42,45 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CryptoTrackerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    val viewModel = koinViewModel<CoinListViewModel>()
-                    val state by viewModel.state.collectAsStateWithLifecycle()
+                val viewModel = koinViewModel<CoinListViewModel>()
+                val state by viewModel.state.collectAsStateWithLifecycle()
+                
+                val context = LocalContext.current
+                val lifecycleOwner = LocalLifecycleOwner.current
+                val snackBarHostState = remember { SnackbarHostState() }
+                val typography = rememberAppTypography()
+
+                ObserveAsEvents(events = viewModel.events) { event ->
+                    when (event) {
+                        is CoinListEvent.Error -> {
+                            snackBarHostState.showSnackbar(
+                                message = event.error.toUiText(context)
+                            )
+                        }
+                    }
+                }
+
+                Scaffold(
+                    snackbarHost = {
+                        SnackbarHost(
+                            hostState = snackBarHostState,
+                            snackbar = { snackBarData ->
+                                Snackbar(
+                                    modifier = Modifier.padding(MaterialTheme.dimens.spaceSmall),
+                                    content = {
+                                        AutoSizeText(
+                                            text = snackBarData.visuals.message,
+                                            adaptiveStyle = typography.bodyMedium,
+                                            maxLines = 2
+                                        )
+                                    }
+                                )
+                            }
+                        )
+                    },
+                    modifier = Modifier.fillMaxSize()) { innerPadding ->
                     CoinListScreen(
                         state = state,
-                        events = viewModel.events,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/CoinListScreen.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/CoinListScreen.kt
@@ -7,104 +7,39 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Snackbar
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.PreviewLightDark
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.compose.LocalLifecycleOwner
-import androidx.lifecycle.repeatOnLifecycle
-import com.plcoding.cryptotracker.core.presentation.util.toUiText
 import com.plcoding.cryptotracker.crypto.presentation.coin_list.components.CoinListItem
 import com.plcoding.cryptotracker.crypto.presentation.coin_list.components.previewCoin
 import com.plcoding.cryptotracker.ui.theme.CryptoTrackerTheme
 import com.plcoding.cryptotracker.ui.theme.dimens
-import com.plcoding.cryptotracker.ui.theme.rememberAppTypography
-import com.plcoding.cryptotracker.util.AutoSizeText
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emptyFlow
-import kotlinx.coroutines.withContext
 
 @Composable
 fun CoinListScreen(state: CoinListState,
-                   events: Flow<CoinListEvent>,
                    modifier: Modifier = Modifier) {
-    val context = LocalContext.current
-    val lifecycleOwner = LocalLifecycleOwner.current
-    val typography = rememberAppTypography()
-    val snackBarHostState = remember { SnackbarHostState() }
-
-    LaunchedEffect(lifecycleOwner.lifecycle){
-        //Listen to the event only when the lifecycle has been started
-        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED){
-            //Ensure that it will actually collect the event.
-            withContext(Dispatchers.Main.immediate){
-                events.collect { event ->
-                    when(event){
-                        is CoinListEvent.Error -> {
-                            // Show error message via snackBarHostState
-                            snackBarHostState.showSnackbar(
-                                message = event.error.toUiText(context)
-                            )
-                        }
-                    }
-                }
-            }
+    if (state.isLoading){
+        Box(modifier = modifier
+            .fillMaxSize(),
+            contentAlignment = Alignment.Center) {
+            CircularProgressIndicator()
         }
     }
-    Scaffold(
-        snackbarHost = {
-            SnackbarHost(
-                hostState = snackBarHostState,
-                snackbar = { snackBarData ->
-                    Snackbar(
-                        modifier = Modifier.padding(MaterialTheme.dimens.spaceSmall),
-                        content = {
-                            AutoSizeText(
-                                text = snackBarData.visuals.message,
-                                adaptiveStyle = typography.bodyMedium,
-                                maxLines = 2
-                            )
-                        }
-                    )
-                }
-            )
-        },
-        modifier = modifier) { padding ->
-        if (state.isLoading){
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .padding(padding),
-                contentAlignment = Alignment.Center) {
-                CircularProgressIndicator()
-            }
-        }
-        else {
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(padding),
-                verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.spaceSmall)) {
-                items(state.coinList) { coinUI ->
-                    CoinListItem(
-                        coinUI = coinUI,
-                        onClick = {},
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                    HorizontalDivider()
-                }
+    else {
+        LazyColumn(
+            modifier = modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.spaceSmall)) {
+            items(state.coinList) { coinUI ->
+                CoinListItem(
+                    coinUI = coinUI,
+                    onClick = {},
+                    modifier = Modifier.fillMaxWidth()
+                )
+                HorizontalDivider()
             }
         }
     }
@@ -124,7 +59,6 @@ private fun CoinListScreenPreview() {
                     )
                 }
             ),
-            events = emptyFlow(),
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.background)
         )
