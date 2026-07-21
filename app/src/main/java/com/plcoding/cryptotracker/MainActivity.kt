87d0acf3f34a9480e404c6094dd47a6ddbab901d
Jirapong Fuangkajonsak <34644844+qjirapong@users.noreply.github.com>
2026-07-21 16:29:05 +0700

    Added events for network error messages

--- app/src/main/java/com/plcoding/cryptotracker/MainActivity.kt
+++ app/src/main/java/com/plcoding/cryptotracker/MainActivity.kt
@@ -30,6 +30,7 @@ class MainActivity : ComponentActivity() {
                     val state by viewModel.state.collectAsStateWithLifecycle()
                     CoinListScreen(
                         state = state,
+                        events = viewModel.events,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/CoinListViewModel.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/CoinListViewModel.kt
@@ -6,11 +6,11 @@ import com.qjirapong.network.onError
 import com.qjirapong.network.onSuccess
 import com.plcoding.cryptotracker.crypto.domain.CoinDataSource
 import com.plcoding.cryptotracker.crypto.presentation.model.toCoinUI
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -26,6 +26,9 @@ class CoinListViewModel(private val coinDataSource: CoinDataSource): ViewModel()
         SharingStarted.WhileSubscribed(5000L),
         CoinListState()
     )
+
+    private val _events = Channel<CoinListEvent>()
+    val events = _events.receiveAsFlow()
 
     fun onAction(action: CoinListAction){
         when(action){
@@ -49,13 +52,15 @@ class CoinListViewModel(private val coinDataSource: CoinDataSource): ViewModel()
                         coinList = coinList.map { coin -> coin.toCoinUI() }
                     )
                 }
-            }.onError {
+            }.onError { error ->
                 _state.update {
                     it.copy(
                         isLoading = false,
                         coinList = emptyList()
                     )
                 }
+                //Send it once, with error details. No sending even if it's config changes.
+                _events.send(CoinListEvent.Error(error))
             }
         }
     }
