647ab06cc22fec2c76485aafef008e049048643b
Jirapong Fuangkajonsak <34644844+qjirapong@users.noreply.github.com>
2026-07-22 15:28:46 +0700

    Added FlowRow to display boxes in Coin Details
    As well as the coin symbol

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_details/CoinDetailsScreen.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_details/CoinDetailsScreen.kt
@@ -2,9 +2,13 @@ package com.plcoding.cryptotracker.crypto.presentation.coin_details
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -17,14 +21,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
+import com.plcoding.cryptotracker.crypto.presentation.coin_details.components.InfoCard
 import com.plcoding.cryptotracker.crypto.presentation.coin_list.CoinListState
 import com.plcoding.cryptotracker.crypto.presentation.coin_list.components.previewCoin
 import com.plcoding.cryptotracker.ui.theme.CryptoTrackerTheme
 import com.plcoding.cryptotracker.ui.theme.dimens
 import com.plcoding.cryptotracker.ui.theme.rememberAppTypography
 import com.plcoding.cryptotracker.util.AutoSizeText
+import com.plcoding.cryptotracker.R
+import com.plcoding.cryptotracker.crypto.presentation.model.toDisplayableNumber
+import com.plcoding.cryptotracker.ui.theme.greenBackground
 
 @Composable
 fun CoinDetailsScreen(state: CoinListState,
@@ -59,9 +68,65 @@ fun CoinDetailsScreen(state: CoinListState,
             )
             AutoSizeText(
                 text = coin.name,
+                adaptiveStyle = typography.headlineMedium,
+                color = contentColor
+            )
+            AutoSizeText(
+                text = coin.symbol,
                 adaptiveStyle = typography.bodyLarge,
                 color = contentColor
             )
+            FlowRow(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center){
+                InfoCard(
+                    title = stringResource(R.string.market_cap),
+                    formattedText = "$ " + coin.marketCapUSD.formatted,
+                    icon = ImageVector.vectorResource(id = R.drawable.stock),
+                    contentColor = contentColor,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center) {
+                    InfoCard(
+                        title = stringResource(R.string.price),
+                        formattedText = "$ " + coin.priceUSD.formatted,
+                        icon = ImageVector.vectorResource(id = R.drawable.dollar),
+                        contentColor = contentColor,
+                        modifier = Modifier.weight(1f)
+                    )
+                    val absChangeFormatted = (
+                            coin.priceUSD.value * (
+                                    coin.changePercent24HR.value / 100
+                            )
+                    ).toDisplayableNumber()
+                    val isPositive = coin.changePercent24HR.value > 0.0
+                    val changeContentColor = if (isPositive){
+                        if (isSystemInDarkTheme()){
+                            Color.Green
+                        }
+                        else{
+                            greenBackground
+                        }
+                    }
+                    else{
+                        MaterialTheme.colorScheme.error
+                    }
+                    InfoCard(
+                        title = stringResource(R.string.change_last_24h),
+                        formattedText = "${if (isPositive) "$ " else "-$ "}${absChangeFormatted.formatted}",
+                        icon = if (isPositive){
+                            ImageVector.vectorResource(id = R.drawable.trending)
+                        }
+                        else{
+                            ImageVector.vectorResource(id = R.drawable.trending_down)
+                        },
+                        contentColor = changeContentColor,
+                        modifier = Modifier.weight(1f)
+                    )
+                }
+            }
         }
     }
 }
